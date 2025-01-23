105066898e3a7bc8b3b484e4f4897a9ecc1c06ff
= <ziabinumer@gmail.com>
2025-01-23 22:00:39 +0500

    add functionality

--- apps/script.js
+++ apps/script.js
@@ -1,3 +1,36 @@
-function addItem() {
-    
+function addItem(title, description) {
+    if (title.value == "") {
+        alert("Title can not be empty.");
+        return;
+    }
+    let list = document.querySelector("#list");
+
+    // create list item
+    let item = document.createElement("li");
+    item.className = "item";
+
+    // create title and description
+    let itemTitle = document.createElement("p");
+    itemTitle.className = "itemTitle";
+    let itemDescription = document.createElement("p");
+    itemDescription.className = "itemDescription";
+
+    itemTitle.innerText = "Title: " + title.value;
+    itemDescription.innerText = description.value;
+
+    // append to item
+    item.appendChild(itemTitle);
+    item.appendChild(itemDescription);
+
+    // append item to list
+    list.appendChild(item);
+
+    // reset input
+    title.value = "";
+    description.value = "";
+    alert('s')
 }
+
+document.querySelector("#submitButton").addEventListener("click", function() {
+    addItem(document.querySelector("#itemName"), document.querySelector("#itemDes"))
+})
